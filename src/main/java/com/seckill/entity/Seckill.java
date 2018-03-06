949e5dfda6de09408016aa6fd33adad7c2d663f3
Yooda <nieyuda@hotmail.com>
2018-03-06 20:35:38 +0900

    none

---
+++ src/main/java/com/seckill/entity/Seckill.java
@@ -0,0 +1,80 @@
+package com.seckill.entity;
+
+import java.util.Date;
+
+/**
+ * @program: izhao
+ * @description: 实体
+ * @author: Mr.Yooda
+ * @create: 2018-03-06 20:24
+ **/
+
+public class Seckill {
+    //表对应Java中的类，比如这个Seckill，而数据库中的列则对应类中的属性
+    private long seckillId;
+    private String name;
+    private int number;
+    private Date startTime;
+    private Date endTime;
+    private Date createTime;
+
+    public long getSeckillId() {
+        return seckillId;
+    }
+
+    public void setSeckillId(long seckillId) {
+        this.seckillId = seckillId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Seckill{" +
+                "seckillId=" + seckillId +
+                ", name='" + name + '\'' +
+                ", number=" + number +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
---
+++ src/main/java/com/seckill/entity/SuccessKilled.java
@@ -0,0 +1,64 @@
+package com.seckill.entity;
+
+import java.util.Date;
+
+/**
+ * @program: izhao
+ * @description: 对应秒杀明细那张表的Java实体类
+ * @author: Mr.Yooda
+ * @create: 2018-03-06 20:32
+ **/
+
+public class SuccessKilled {
+
+    private long seckillId;
+
+    private long userPhone;
+
+    private short state;
+
+    private Date createTime;
+
+    public long getSeckillId() {
+        return seckillId;
+    }
+
+    public void setSeckillId(long seckillId) {
+        this.seckillId = seckillId;
+    }
+
+    public long getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(long userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public short getState() {
+        return state;
+    }
+
+    public void setState(short state) {
+        this.state = state;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "SuccessKilled{" +
+                "seckillId=" + seckillId +
+                ", userPhone=" + userPhone +
+                ", state=" + state +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
+
